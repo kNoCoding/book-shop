550477313bc1f0a9c6282d762295ac9404f0f763
Konstantin Nochevny <kosnobiz@gmail.com>
2023-11-19 20:58:10 +0200

    Populate model private functions

--- js/services/book-store.service.js
+++ js/services/book-store.service.js
@@ -21,11 +21,31 @@ function setBookFilter(filterBy) { }
 
 function setBookSort(sortBy) { }
 
+
+
 // private functions
 
-function _createBook() { }
+function _createBook(title, price) {
+    return {
+        id: makeId(4),
+        title,
+        price,
+    }
+}
 
-function _createBooks() { }
+function _createBooks() {
+    gBooks = loadFromStorage(STORAGE_KEY)
+    //if gBooks exists and isnt empty dont populate with demo data
+    if (gBooks && gBooks.length) return
+
+    gBooks = [
+        _createBook('Learning Laravel', 18.90),
+        _createBook('Beginning with Laravel', 6.65),
+        _createBook('Java for developers', 7.20),
+    ]
+
+    _saveBooksToStorage()
+}
 
 function _saveBooksToStorage() {
     saveToStorage(STORAGE_KEY, gBooks)
